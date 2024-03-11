cbeb1fdc5a09f56f712e88e03bc10294b2328d65
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-11 12:18:09 +0400

    React redux setup

---
+++ react-redux-demo/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CakeContainer from "./components/CakeContainer";
+
+function App() {
+  return (
+    <div className="app">
+      <CakeContainer />
+    </div>
+  );
+}
+
+export default App;
